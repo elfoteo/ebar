1aaefe4b7a0710812f64637d6d90f02528fa028e
elfoteo <matteociocci08@gmail.com>
2026-05-24 08:45:34 +0100

    Notify if build fails

--- launch.sh
+++ launch.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if ! make -C "$DIR"; then
+    notify-send "ebar compilation failed"
+    exit 1
+fi
+
 killall ebar
-make -C "$DIR"
 "$DIR/build/ebar" &
